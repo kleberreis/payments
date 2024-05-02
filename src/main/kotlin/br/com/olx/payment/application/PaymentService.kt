050d2e2073094f7d8ec9f9e6e65c51ff0e3745f7
Rachel Castro <rachel.castro@olxbr.com>
2024-05-02 10:07:07 -0300

    wip: setting up kafka

--- src/main/kotlin/br/com/olx/payment/application/PaymentService.kt
+++ src/main/kotlin/br/com/olx/payment/application/PaymentService.kt
@@ -9,35 +9,47 @@ import jakarta.transaction.Transactional
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
+import org.springframework.kafka.core.KafkaTemplate
 
 
 @Service
 class PaymentService(
     @Autowired
-    val repository: ActivationRepository
+    val repository: ActivationRepository,
+    @Value("\${kafka.topics.product}") val topic: String,
+    @Autowired
+    private val kafkaTemplate: KafkaTemplate<String, Any>
 ) {
-
+    
     @Transactional
     fun save(payment: Payment){
         val entity = Payments.from(payment)
         repository.save(entity)
-        try{
-            sendToActivation(payment)
-        }catch (e: Exception){
+        //try{
+            //sendToActivation(payment)
+            mandaPraFila()
+        //}catch (e: Exception){
             println("mensagem não enviada para activations")
-        }
+        //}
     }
 
     fun mandaPraFila(){
+        val message: Message<String> = MessageBuilder
+                .withPayload("mensagem")
+                .setHeader(KafkaHeaders.TOPIC, topic)
+                .setHeader("X-Custom-Header", "Custom header here")
+                .build()
+        kafkaTemplate.send(message)
+        log.info("Message sent with success")
         println("mandei pra fila")
         throw RuntimeException("lascou!")
     }
 
-    fun sendToActivation(payment: Payment) {
+    /*fun sendToActivation(payment: Payment) {
         val createActivationUrl = "http://localhost:8080"
         val restTemplate = RestTemplate()
         val activation = Activation(payment.productKey, payment.adId)
 
         restTemplate.postForEntity(createActivationUrl, activation, String.javaClass)
-    }
+    }*/
 }
